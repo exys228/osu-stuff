6d159dbde355c891ab76de5343379b2cb1e051cd
exys228 <exys228@gmail.com>
2019-10-02 01:45:04 +0300

    Given class/methods/fields etc. appropriate modifiers, added handler for unhandled exceptions, made program more informative as a CLI, added mini-about and simple how to use, better exception handling and exiting with notifying about error, PatchResult(s) are now displayed after patching if exception(s) happened, suggesting user to cancel by pressing (y/n) if some patches have failed.

--- osu!patch/Program.cs
+++ osu!patch/Program.cs
@@ -13,84 +13,109 @@ using System.Reflection;
 using de4dot.code;
 using de4dot.code.AssemblyClient;
 using de4dot.code.deobfuscators;
+using osu_patch.Misc;
 using StringFixerMini;
+
 using LdstrOccurence = System.Tuple<dnlib.DotNet.Emit.CilBody, int>;
 
 namespace osu_patch
 {
 	static class CMain
 	{
-		public static ModuleDefMD ObfOsuModule;
-		public static ModuleDefMD CleanOsuModule;
+		private static ModuleDefMD _obfOsuModule;
+		private static ModuleDefMD _cleanOsuModule;
 
-		public static Assembly ObfOsuAssembly;
+		private static Assembly _obfOsuAssembly;
 
-		public static ModuleExplorer ObfOsuExplorer;
+		private static ModuleExplorer _obfOsuExplorer;
 
-		public static string ObfOsuPath = "";
-		public static string CleanOsuPath = "";
+		private static string _obfOsuPath = "";
+		private static string _cleanOsuPath = "";
 
 		public static string ObfOsuHash = "";
 
-		public static readonly string ExecutingAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+		private static readonly string ExecutingAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-		static int Main(string[] args)
-		{
-			// Console.ReadKey(true);
+		public static int Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
+                Message("[MAIN]: Unhandled exception! This shouldn't occur. Details:\n" + eventArgs.ExceptionObject);
 
-			if (args.Length < 2 || !File.Exists(CleanOsuPath = Path.GetFullPath(args[0])) || !File.Exists(ObfOsuPath = Path.GetFullPath(args[1])))
-				return 1;
+            // Console.ReadKey(true);
+
+            if (args.Length < 2)
+				return Exit("osu!patch - osu! assembly patcher based on NameMapper\n" +
+							   "by exys, 2019\n" +
+							   "\n" +
+							   "Usage:\n" +
+							   "osu!patch [clean module] [obfuscated module]");
+
+			if (!File.Exists(_cleanOsuPath = Path.GetFullPath(args[0])))
+				return Exit("F | Specified clean module path does not exist!\n");
+
+			if (!File.Exists(_obfOsuPath = Path.GetFullPath(args[1])))
+				return Exit("F | Specified obfuscated module path does not exist!\n");
 
 			try
 			{
-				ObfOsuModule = ModuleDefMD.Load(ObfOsuPath);
-				CleanOsuModule = ModuleDefMD.Load(CleanOsuPath);
+				_obfOsuModule = ModuleDefMD.Load(_obfOsuPath);
+				_cleanOsuModule = ModuleDefMD.Load(_cleanOsuPath);
 
-				ObfOsuAssembly = Assembly.LoadFile(ObfOsuPath);
+				_obfOsuAssembly = Assembly.LoadFile(_obfOsuPath);
 			}
-			catch { return 1; }
+			catch (Exception ex) { return Exit("F | Unable to load one of the modules! Details:\n" + ex); }
 
-			ObfOsuHash = MD5Helper.Compute(ObfOsuPath); // ORIGINAL!!!!!! hash, PLEASE PASS UNMODIFIED PEPPY-SIGNED ASSEMBLY AS ObfOsuModule!@!!32R1234 (refer to "Patcher addon" patch)
+			ObfOsuHash = MD5Helper.Compute(_obfOsuPath); // ORIGINAL!!!!!! hash, PLEASE PASS UNMODIFIED PEPPY-SIGNED ASSEMBLY AS _obfOsuModule!@!!32R1234 (refer to "Patcher addon" patch)
 
 			// --- Cleaning control flow!
 
-			var options = new ObfuscatedFile.Options
+			try
 			{
-				Filename = ObfOsuPath, // will this work or do i need filename ONLY? yes it will ok
-				ControlFlowDeobfuscation = true,
-				KeepObfuscatorTypes = true,
-				RenamerFlags = 0,
-				StringDecrypterType = DecrypterType.None,
-				MetadataFlags = MetadataFlags.PreserveRids |
-				    MetadataFlags.PreserveUSOffsets |
-				    MetadataFlags.PreserveBlobOffsets |
-				    MetadataFlags.PreserveExtraSignatureData
-			};
+				var options = new ObfuscatedFile.Options
+				{
+					Filename = _obfOsuPath, // will this work or do i need filename ONLY? yes it will ok
+					ControlFlowDeobfuscation = true,
+					KeepObfuscatorTypes = true,
+					RenamerFlags = 0,
+					StringDecrypterType = DecrypterType.None,
+					MetadataFlags = MetadataFlags.PreserveRids |
+									MetadataFlags.PreserveUSOffsets |
+									MetadataFlags.PreserveBlobOffsets |
+									MetadataFlags.PreserveExtraSignatureData
+				};
 
-			var obfFile = new ObfuscatedFile(options, new ModuleContext(TheAssemblyResolver.Instance), new NewAppDomainAssemblyClientFactory());
+				var obfFile = new ObfuscatedFile(options, new ModuleContext(TheAssemblyResolver.Instance), new NewAppDomainAssemblyClientFactory());
 
-			obfFile.DeobfuscatorContext = new DeobfuscatorContext();
-			obfFile.Load(new List<IDeobfuscator> { new de4dot.code.deobfuscators.Unknown.DeobfuscatorInfo().CreateDeobfuscator() });
+				obfFile.DeobfuscatorContext = new DeobfuscatorContext();
+				obfFile.Load(new List<IDeobfuscator> { new de4dot.code.deobfuscators.Unknown.DeobfuscatorInfo().CreateDeobfuscator() });
 
-			obfFile.DeobfuscateBegin();
-			obfFile.Deobfuscate();
-			obfFile.DeobfuscateEnd();
+				obfFile.DeobfuscateBegin();
+				obfFile.Deobfuscate();
+				obfFile.DeobfuscateEnd();
 
-			ObfOsuModule = obfFile.ModuleDefMD;
+				_obfOsuModule = obfFile.ModuleDefMD;
+			}
+			catch (Exception ex) { return Exit("F | Unable to deobfuscate control flow of obfuscated assembly! Details:\n" + ex); }
 
 			// ---
 
 			// fixin' strings real quick
-			StringFixer.Fix(ObfOsuModule, ObfOsuAssembly);
+			try
+			{
+				StringFixer.Fix(_obfOsuModule, _obfOsuAssembly);
+			}
+			catch (Exception ex) { return Exit("F | Unable to fix strings of obfuscated assembly! Details:\n" + ex); }
 
 #if DEBUG
-			ObfOsuModule.Write(Path.Combine(Path.GetDirectoryName(ObfOsuPath), "OsuObfModule-cflow-string.exe"), new ModuleWriterOptions(ObfOsuModule)
+			_obfOsuModule.Write(Path.Combine(Path.GetDirectoryName(_obfOsuPath), "OsuObfModule-cflow-string.exe"), new ModuleWriterOptions(_obfOsuModule)
 			{
 				MetadataOptions = { Flags = MetadataFlags.KeepOldMaxStack }
 			});
 #endif
 
-			Message($"[MAIN]: Loaded assemblies: {CleanOsuModule.Assembly.FullName} (clean); {ObfOsuModule.Assembly.FullName} (obfuscated).");
+			Message($"[MAIN]: Loaded assemblies: {_cleanOsuModule.Assembly.FullName} (clean); {_obfOsuModule.Assembly.FullName} (obfuscated).");
+
+			// --- Working with names
 
 			var cacheFolderName = Path.Combine(ExecutingAssemblyLocation, "cache");
 
@@ -102,73 +127,129 @@ namespace osu_patch
 
 			var dictFile = Path.Combine(cacheFolderName, $"{ObfOsuHash}.dic");
 
-#if !DEBUG
-			if (File.Exists(dictFile))
+			try
 			{
-				Message("[MAIN]: Found cached namedict file for this assembly! Loading names...");
+// #if !DEBUG
+				if (File.Exists(dictFile))
+				{
+					Message("[MAIN]: Found cached namedict file for this assembly! Loading names...");
 
-				var nameProvider = SimpleNameProvider.Initialize(File.ReadAllBytes(dictFile));
-				ObfOsuExplorer = new ModuleExplorer(ObfOsuModule, nameProvider);
-			}
-			else
-#endif
-			{
-				Message("[MAIN]: No cached dict found! Initializing DefaultNameProvider (NameMapper)...");
+					var nameProvider = SimpleNameProvider.Initialize(File.ReadAllBytes(dictFile));
+					_obfOsuExplorer = new ModuleExplorer(_obfOsuModule, nameProvider);
+				}
+				else
+// #endif
+				{
+					Message("[MAIN]: No cached dict found! Initializing DefaultNameProvider (NameMapper)...");
 
 #if DEBUG
-				TextWriter debugOut = Console.Out;
+					TextWriter debugOut = Console.Out;
 #else
-				TextWriter debugOut = null;
+					TextWriter debugOut = null;
 #endif
 
-				DefaultNameProvider.Initialize(CleanOsuModule, ObfOsuModule, debugOut);
-				File.WriteAllBytes(dictFile, DefaultNameProvider.Instance.Pack());
-				ObfOsuExplorer = new ModuleExplorer(ObfOsuModule);
+					MapperNameProvider.Initialize(_cleanOsuModule, _obfOsuModule, debugOut);
+					File.WriteAllBytes(dictFile, MapperNameProvider.Instance.Pack());
+					_obfOsuExplorer = new ModuleExplorer(_obfOsuModule);
+				}
 			}
+			catch (Exception ex) { return Exit("F | Unable to get clean names for obfuscated assembly! Details:\n" + ex); }
+
+			// ---
 
 			Message("[MAIN]: Done! Now patching.");
 
 			bool overallSuccess = true;
 
+			var failedDetails = new List<PatchResult>();
+
 			foreach (var patch in Patches.PatchList)
 			{
 				Console.Write($"[MAIN]: {patch.Name}: ");
-				bool success = patch.Execute();
 
-				if (!success)
+                PatchResult res = patch.Execute(_obfOsuExplorer);
+
+                switch (res.Result)
 				{
-					if (!patch.Enabled)
-					{
+					case PatchStatus.Disabled:
 						Console.ForegroundColor = ConsoleColor.Gray;
 						Message("DISABLED");
-					}
-					else
-					{
+						break;
+
+					case PatchStatus.Exception:
+					case PatchStatus.Failure:
 						Console.ForegroundColor = ConsoleColor.Red;
 						Message("FAIL");
+						failedDetails.Add(res);
 						overallSuccess = false;
-					}
-				}
-				else
-				{
-					Console.ForegroundColor = ConsoleColor.Green;
-					Message("DONE");
+						break;
+
+					case PatchStatus.Success:
+						Console.ForegroundColor = ConsoleColor.Green;
+						Message("DONE");
+						break;
+
+					default:
+						Console.ForegroundColor = ConsoleColor.DarkGray;
+						Message("[???]");
+						break;
 				}
 
 				Console.ResetColor();
 			}
 
-			string filename = Path.GetFileNameWithoutExtension(ObfOsuPath) + "-osupatch" + Path.GetExtension(ObfOsuPath);
+			if (failedDetails.Any())
+				Message("[MAIN]: There's some details about failed patches.");
+
+			foreach (var details in failedDetails)
+			{
+				details.PrintDetails(Console.Out);
+				Message();
+			}
+
+            if (!overallSuccess)
+            {
+                Console.WriteLine("[MAIN]: There are some failed patches. Do you want to continue?");
+                Console.WriteLine("[MAIN]: Warning: in case of self-update pressing N will leave stock version of osu! without patching it!");
+                Console.Write("Waiting for user input: (y/n) ");
+
+                var exit = false;
+
+                while (true)
+                {
+                    var key = Console.ReadKey(true).Key;
+
+                    if (key == ConsoleKey.Y)
+                        break;
+                    
+                    if (key == ConsoleKey.N)
+                    {
+                        exit = true;
+                        break;
+                    }
+                }
+
+                Message();
+
+                if(exit)
+                    return Exit("[MAIN]: Aborted by user.");
+            }
+
+			string filename = Path.GetFileNameWithoutExtension(_obfOsuPath) + "-osupatch" + Path.GetExtension(_obfOsuPath);
 
 			Message($"[MAIN]: Saving assembly as {filename}");
 
-			ObfOsuModule.Write(Path.Combine(Path.GetDirectoryName(ObfOsuPath), filename), new ModuleWriterOptions(ObfOsuModule)
+			try
 			{
-				MetadataOptions = { Flags = MetadataFlags.KeepOldMaxStack }
-			});
+				_obfOsuModule.Write(Path.Combine(Path.GetDirectoryName(_obfOsuPath), filename), new ModuleWriterOptions(_obfOsuModule)
+				{
+					MetadataOptions = { Flags = MetadataFlags.KeepOldMaxStack }
+				});
+			}
+			catch (Exception ex) { return Exit("F | Unable to save patched assembly! Details:\n" + ex); }
 
-			CleanOsuModule.Dispose();
-			ObfOsuModule.Dispose();
+			_cleanOsuModule.Dispose();
+			_obfOsuModule.Dispose();
 
 #if DEBUG
 				Console.ReadKey(true);
@@ -177,38 +258,25 @@ namespace osu_patch
 			return overallSuccess ? 0 : 1;
 		}
 
-		private static int Message(string msg)
-		{
-			Console.WriteLine(msg);
-			return 1;
-		}
+        private static int Message(string msg = "")
+        {
+            Console.WriteLine(msg);
+            return 1;
+        }
 
-		private static int[] Range(int startIndex, int count)
-		{
-			int[] result = new int[count];
+#if DEBUG
+        private static int Exit(string msg = "")
+        {
+            Console.WriteLine(msg + "\n-=-=-=-=-=-=-=-=-=-=- EXIT EXIT EXIT");
+            Console.ReadKey(true);
+            return 1;
+        }
+#else
+        private static int Exit(string msg = "") => Message(msg);
+#endif
+    }
 
-			for (int i = 0; i < count; i++)
-			{
-				result[i] = startIndex++;
-			}
-
-			return result;
-		}
-
-		public static OsuFindCollection FindTypeMethod(string typeName, string methodName)
-		{
-			try
-			{
-				var type = ObfOsuExplorer[typeName];
-				var method = type[methodName];
-
-				return new OsuFindCollection(type.Type, method.Method, true);
-			}
-			catch { return new OsuFindCollection(null, null, false); }
-		}
-	}
-
-	public static class OsuPatchExtensions
+    public static class OsuPatchExtensions
 	{
 		/// <summary>
 		/// o no so old much deprecated use ModuleExplorer->TypeExplorer->MethodExplorer->MethodEditor when possible
@@ -227,28 +295,28 @@ namespace osu_patch
 				originalArray.Insert(index, instructions[i]);
 		}
 
-		public static MemberRef CreateMethodRef(this ModuleDefMD module, bool isStatic, Type type, string methodName, Type returnType, params Type[] argsType)
+		public static MemberRef CreateMethodRef(this ModuleDef module, bool isStatic, Type type, string methodName, Type returnType, params Type[] argsType)
 		{
-			TypeRefUser typeRef = type.GetTypeRef();
+			TypeRefUser typeRef = type.GetTypeRef(module);
 
-			TypeSig returnSig = returnType.GetTypeSig();
+			TypeSig returnSig = returnType.GetTypeSig(module);
 			TypeSig[] argsSig = new TypeSig[argsType.Length];
 
 			for (int i = 0; i < argsSig.Length; i++)
-				argsSig[i] = argsType[i].GetTypeSig();
+				argsSig[i] = argsType[i].GetTypeSig(module);
 
 			MethodSig methodSig = isStatic ? MethodSig.CreateStatic(returnSig, argsSig) : MethodSig.CreateInstance(returnSig, argsSig);
 
-			MemberRefUser methodRef = new MemberRefUser(CMain.ObfOsuModule, methodName, methodSig, typeRef);
+			MemberRefUser methodRef = new MemberRefUser(module, methodName, methodSig, typeRef);
 
 			return methodRef;
 		}
 
 		public static TypeDef GetTypeDef(this FieldDef field) => field.FieldType.ToTypeDefOrRef().ResolveTypeDef();
 
-		public static TypeSig GetTypeSig(this Type type) => GetTypeRef(type).ToTypeSig();
+		public static TypeSig GetTypeSig(this Type type, ModuleDef module) => type.GetTypeRef(module).ToTypeSig();
 
-		public static TypeRefUser GetTypeRef(this Type type)
+		public static TypeRefUser GetTypeRef(this Type type, ModuleDef module)
 		{
 			string nameSpace = null, typeName = type.FullName;
 			int idx = typeName.LastIndexOf('.');
@@ -259,7 +327,7 @@ namespace osu_patch
 				typeName = typeName.Substring(idx + 1);
 			}
 
-			return new TypeRefUser(CMain.ObfOsuModule, nameSpace, typeName, CMain.ObfOsuModule.Import(type).DefinitionAssembly.ToAssemblyRef());
+			return new TypeRefUser(module, nameSpace, typeName, module.Import(type).DefinitionAssembly.ToAssemblyRef());
 		}
 
 		public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
