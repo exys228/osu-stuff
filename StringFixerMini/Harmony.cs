d6cb2c64cd89ae029b7564d5d58e6f140fa98f9e
exys228 <exys228@gmail.com>
2019-10-03 17:20:09 +0300

    Removed unnecessary debug messages

--- StringFixerMini/Harmony.cs
+++ StringFixerMini/Harmony.cs
@@ -25,11 +25,8 @@ namespace StringFixerMini
 			public static void Postfix(ref MethodBase __result)
 			{
 				if (__result.DeclaringType == typeof(RuntimeMethodHandle))
-				{
-					__result = MethodToReplace ?? MethodBase.GetCurrentMethod();
-					Console.WriteLine("[Harmony] Patched stacktrace entry");
-				}
-			}
+                    __result = MethodToReplace ?? MethodBase.GetCurrentMethod();
+            }
 		}
 	}
 }
