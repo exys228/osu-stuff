cd222f43203bd18508ec059aff9b98ec586ef7fa
exys228 <exys228@gmail.com>
2019-10-04 17:19:35 +0300

    Added OsuVersionDownloader and two empty projects (todo)

---
+++ OsuPatchPlugin.SilentOsuDirect/SilentOsuDirectPlugin.cs
@@ -0,0 +1,33 @@
+﻿using dnlib.DotNet;
+using osu_patch;
+using osu_patch.Custom;
+using osu_patch.Misc;
+using System.Collections.Generic;
+using dnlib.DotNet.Emit;
+
+namespace OsuPatchPlugin.SilentOsuDirect
+{
+	public class SilentOsuDirectPlugin : IOsuPatchPlugin
+	{
+		public IEnumerable<Patch> GetPatches() => new[]
+		{
+			new Patch("UpdateStatus patch", true, (patch, exp) =>
+			{
+				var editor = exp["osu.Online.BanchoClient"]["UpdateStatus"].Editor;
+
+				var loc = editor.Locate(new[]
+				{
+					OpCodes.Ldc_I4_S, 
+					OpCodes.Starg_S, 
+					OpCodes.Br_S, 
+					OpCodes.Call, 
+					OpCodes.Brfalse_S, 
+				});
+
+				return new PatchResult(patch, PatchStatus.Success);
+			}),
+		};
+		
+		public void Load(ModuleDef originalObfOsuModule) { }
+	}
+}
---
+++ OsuVersionDownloader/MD5Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace OsuVersionDownloader
+{
+    class MD5Helper
+    {
+        public static string Compute(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    stream.Close();
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+    }
+}
