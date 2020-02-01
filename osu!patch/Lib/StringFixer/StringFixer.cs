21e42f73a8d00539d8dc79106e3dae415d7f59e4
exys <no-email-sorry ;c>
2020-02-01 03:54:32 +0300

    Forgot to add license. Holly must be hating me now ;_;

--- osu!patch/Lib/StringFixer/StringFixer.cs
+++ osu!patch/Lib/StringFixer/StringFixer.cs
@@ -1,4 +1,27 @@
-﻿using dnlib.DotNet;
+﻿/* MIT License
+ *
+ * Copyright (c) 2017 HoLLy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using dnlib.DotNet;
 using dnlib.DotNet.Emit;
 using System;
 using System.Collections.Generic;
@@ -104,7 +127,7 @@ namespace osu_patch.Lib.StringFixer
 					yield return m;
 		}
 
-		private static MethodInfo FindMethod(System.Reflection.Assembly ass, MethodDef meth, Type[] args)
+		private static MethodInfo FindMethod(Assembly ass, MethodDef meth, Type[] args)
 		{
 			var flags = BindingFlags.Default;
 			flags |= meth.IsPublic ? BindingFlags.Public : BindingFlags.NonPublic;
